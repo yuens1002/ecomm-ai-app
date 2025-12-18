cdd21a45323d285ee52fb9a690a453374907cc2a
sunny yuen <yuens1002@gmail.com>
2025-12-18 14:30:57 -0500

    fix: remove unused useEffect import

--- app/auth/signin/signin-content.tsx
+++ app/auth/signin/signin-content.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { signIn } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
